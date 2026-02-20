fe7f4d9d33dd21e5e58ffc9b354e393587ef11a7
Kyame Israel <kyame.israel@gmail.com>
2026-02-20 00:50:38 -0500

    Add constructors, getters, and deposit/withdraw to Account.java

---
+++ src/main/Account.java
@@ -0,0 +1,45 @@
+public class Account {
+
+    private String name;
+    private String accountNumber;
+    private String password;
+    private double balance;
+
+    public Account(String name, String accountNumber, String password, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean validatePassword(String password) {
+        return this.password.equals(password);
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && this.balance >= amount) {
+            this.balance -= amount;
+        }
+        else  {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+    }
+}
